5a47a674e3a658409ed58ed1f43ff684db37c0ef
amrutha <amruthatc86@gmail.com>
2024-05-08 18:51:37 +0400

    first commit

---
+++ responsive-nav/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import './App.css'
+import Dashboard from './components/Dashboard'
+import Sidebar from './components/Sidebar'
+
+function App() {
+  const [sidebarToggle, setSidebarToggle] = useState(false)
+
+  return (
+    <div className='flex'>
+      <Sidebar sidebarToggle={sidebarToggle}/>
+      <Dashboard
+      sidebarToggle={sidebarToggle}
+      setSidebarToggle={setSidebarToggle}/>
+    </div>
+  )
+}
+
+export default App
